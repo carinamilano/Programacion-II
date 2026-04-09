ee6d13052a26d77fc458a600cbc6a2f0f1e64644
Carina Milano <carinamilano@gmail.com>
2026-04-09 16:42:40 -0300

    ..

---
+++ Colas/src/Main2.java
@@ -0,0 +1,30 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        BuffetFacu bufet = new BuffetFacu();
+        bufet.InicializarCola();
+
+        System.out.println("********Buffet de la Facultad********");
+        System.out.println("Llegan a la fila!!");
+        bufet.Acolar(1);
+        System.out.println("Persona 1 se pone en la fila");
+        System.out.println("Llegan a la fila!!");
+        bufet.Acolar(2);
+        System.out.println("Persona 2 se pone en la fila");
+        System.out.println("Llegan a la fila!!");
+        bufet.Acolar(3);
+        System.out.println("Persona 3 se pone en la fila");
+        //se atiende al primero que llego
+        System.out.println("Se atiende a la persona: " + bufet.Primero());
+        bufet.Desacolar();
+        System.out.println("Se atiende a la persona: " + bufet.Primero());
+        bufet.Desacolar();
+        System.out.println("Se atiende a la persona: " + bufet.Primero());
+        bufet.Desacolar();
+        if (bufet.ColaVacia()){
+            System.out.println("no hay mas gente en la fila");
+        }
+
+
+    }
+}
