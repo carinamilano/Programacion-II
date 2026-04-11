8f674488c9a5d811dedc15110699e26fe5ab41db
Carina Milano <carinamilano@gmail.com>
2026-04-11 08:05:39 -0300

    fila cajero

--- ColasConPrioridad/src/Main3.java
+++ ColasConPrioridad/src/Main3.java
@@ -5,11 +5,9 @@ public class Main3 {
         fila.InicializarColaPrioridad();
 
         // Acolamos 3 personas
-        System.out.println("Llega al banco la persona 1 con prioridad 1");
+        System.out.println("Llegan al banco:  \n persona 1 (prioridad 1) / persona 2 (prioridad 5) / persona 3 (prioridad 10)");
         fila.AcolarPrioridad(1, 1);
-        System.out.println("Llega al banco la persona 2 con prioridad 5");
         fila.AcolarPrioridad(2, 5);
-        System.out.println("Llega al banco la persona 3 con prioridad 10");
         fila.AcolarPrioridad(3, 10); //la persona que llego 3era es la de mayor prioridad
 
         // Mostramos cómo salen del banco!!
